96313ddb19f70a75c184fcc62374807d9f54b90c
Joker2770 <1214220480@qq.com>
2018-12-24 16:56:31 +0800

    replace SPACE as TAB

--- src/PerceptronLearnRule.py
+++ src/PerceptronLearnRule.py
@@ -45,19 +45,19 @@ b = 0
 
 # 循环一个比较大的次数，比如100
 for i in range(100):
-   # 打印权值
-   print(w0,w1,w2)
-   # 计算感知器的输出
-   y = np.sign(w0 * x0 + w1 * x1 + w2*x2)
-   # 如果感知器输出不等于正确的标签
-   if(y != t):
-       # 更新权值
-       w0 = w0 + lr * (t-y) * x0
-       w1 = w1 + lr * (t-y) * x1
-       w2 = w2 + lr * (t-y) * x2
-   # 如果感知器输出等于正确的标签
-   else:
-       # 训练结束
-       print('done')
-       # 退出循环
-       break
+	# 打印权值
+	print(w0,w1,w2)
+	# 计算感知器的输出
+	y = np.sign(w0 * x0 + w1 * x1 + w2*x2)
+	# 如果感知器输出不等于正确的标签
+	if(y != t):
+		# 更新权值
+		w0 = w0 + lr * (t-y) * x0
+		w1 = w1 + lr * (t-y) * x1
+		w2 = w2 + lr * (t-y) * x2
+	# 如果感知器输出等于正确的标签
+	else:
+		# 训练结束
+		print('done')
+		# 退出循环
+		break
